80060f16c966741f5526567bd781cb16b9d94b07
Rome <romero.guzman18@gmail.com>
2025-04-23 12:59:28 -0500

    rescursive functions

--- 08-functions.go
+++ 08-functions.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Functios variadica
+// Functios variadica -----------------------
 // cons ...tipo - se le dice que reciba n cantidad de parametros y su tipo
 func sum(name string, nums ...int) int {
 	total := 0
@@ -14,13 +14,25 @@ func sum(name string, nums ...int) int {
 	return total
 }
 
+// send multiple data
 func returnData(data ...interface{}) {
 	for _, dato := range data {
 		fmt.Printf("%T - %v\n", dato, dato) //%T -> tipo de dato
 	}
 }
 
+// Recursive functions -----------------
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
 func Functions() {
 	fmt.Println(sum("pepito", 2, 3))
 	returnData("Hola", 42, true, 3.14)
+
+	//recursive
+	fmt.Println("Recursive function ->", factorial(5))
 }
